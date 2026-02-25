4ec092b8d6d0eec6810a600fdf791b64a1e48b7b
kribbany <palyi.kristof@gmail.com>
2026-02-25 14:41:55 +0100

    feat(UploadPage): refactor file upload mutation to use object syntax

--- src/pages/upload.tsx
+++ src/pages/upload.tsx
@@ -20,7 +20,9 @@ const UploadPage = () => {
       return;
     }
 
-    uploadFilesMutation.mutate(files);
+    uploadFilesMutation.mutate({
+      files,
+    });
   };
 
   const handleCardZoom = (itemId: string) => {
