8d72eed881baffa17bd567043540034389199c60
Kribbany <palyi.kristof@gmail.com>
2026-06-24 13:10:26 +0200

    fix

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,8 +27,8 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('')`. */
-    // baseURL: "https://hyperion-frontend-blond.vercel.app",
-    baseURL: "http://localhost:5173",
+    baseURL: "https://hyperion-frontend-blond.vercel.app",
+    // baseURL: "http://localhost:5173",
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
