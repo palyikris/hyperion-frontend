e05e0ef841cbf6b0478db14bb75423412365b097
Kribbany <palyi.kristof@gmail.com>
2026-06-24 13:19:21 +0200

    fix

--- global-teardown.ts
+++ global-teardown.ts
@@ -1,14 +1,15 @@
-import { chromium } from "@playwright/test";
+import { chromium, FullConfig } from "@playwright/test";
 import { VaultPage } from "./tests/pages/VaultPage";
 import path from "path";
 
 async function globalTeardown() {
   const storageState = path.join("playwright", ".auth", "user.json");
+  const baseURL = "https://hyperion-frontend-blond.vercel.app";
   const browser = await chromium.launch();
   const page = await browser.newPage({ storageState });
 
   const vaultPage = new VaultPage(page);
-  await vaultPage.goto();
+  await page.goto(baseURL + "/vault");
   await vaultPage.waitForGalleryResponse();
 
   const initialCount = await vaultPage.getGalleryImageCardCount();
